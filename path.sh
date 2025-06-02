2a2c78b561e86f0bf25103c5067c0b934191b80b
Jie-shiang <snoopy963852@gmail.com>
2025-06-02 15:08:51 +0800

    Initial commit

--- path.sh
+++ path.sh
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # ADReSS-IS2020-data
-export CPFL_codeRoot=/home/jieshiang/Desktop/CharDiv-clustered-CPFL-main-copy
-export CPFL_dataRoot=/home/jieshiang/Desktop/CharDiv-clustered-CPFL-main-copy/ADReSS-IS2020-data
+export CPFL_codeRoot=
+export CPFL_dataRoot=
 
 # ADOS
-# export CPFL_codeRoot=/home/jieshiang/Desktop/CharDiv-clustered-CPFL-main
-# export CPFL_dataRoot=/home/jieshiang/Desktop/CharDiv-clustered-CPFL-main/ADOS_copy
+# export CPFL_codeRoot=
+# export CPFL_dataRoot=
 
 # Show CPFL_codeRoot
 echo "CPFL_codeRoot is set to: $CPFL_codeRoot"
--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ export CUDA_VISIBLE_DEVICES=3
 python src/federated_main.py \
     --model=data2vec \
     --gpu=0 \
-    --pretrain_name "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h-local/" \
+    --pretrain_name "./data2vec-audio-large-960h-local/" \
     --frac=1.0 \
     --num_users=5 \
     --global_ep=10 \
@@ -23,11 +23,11 @@ python src/federated_main.py \
     --local_ep 10 \
     --epochs 10 \
     --N_Kmeans_update 10 \
-    --FL_STAGE 3 \
-    -model_out "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h" \
-    -model_in "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h" \
-    --dataset_path_root "/mnt/Internal/jieshiang/ADReSS-copy/datasets" \
-    --Kmeans_model_path "/mnt/Internal/jieshiang/ADReSS-copy/save/k_means_model" \
+    --FL_STAGE 1 \
+    -model_out "./data2vec-audio-large-960h" \
+    -model_in "./data2vec-audio-large-960h" \
+    --dataset_path_root "./ADReSS-copy/datasets" \
+    --Kmeans_model_path "./save/k_means_model" \
     --eval_mode 2 \
     --FL_type 1 \
     --mu 0.01 \
@@ -43,77 +43,6 @@ python src/federated_main.py \
     # --FML_model 0 \
     # --CBFL \
     # --WeightedAvg \
-
-# FL algorithm types:
-# FL           : FL_type 1 / num_lms 1
-# FL_weighted  : FL_type 1 / num_lms 1 / WeightedAvg
-# FedProx      : FL_type 2 / num_lms 1
-# FML(Local)   : FL_type 3 / num_lms 1 / FML_model 0 
-# FML(mutual)  : FL_type 3 / num_lms 1 / FML_model 1
-# CBFL-embs    : FL_type 1 / num_lms 7 / CBFL
-# CPFL-CharDiv : FL_type 1 / num_lms 7
-
-# Handle SIGINT and SIGTERM for graceful termination of background processes
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
-
-# Wait for all background processes to complete
-wait
-
-
-#!/bin/bash
-
-# Uncomment the line below if you need to set CUDA memory allocation configurations
-export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:32"
-
-# Source the path configurations
-. ./path.sh
-
-# Setting CUDA device to use
-export CUDA_VISIBLE_DEVICES=3
-
-# Running the Python script with specified arguments for Stage 4 (FL Training)
-python src/federated_main.py \
-    --model=data2vec \
-    --gpu=0 \
-    --pretrain_name "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h-local/" \
-    --frac=1.0 \
-    --num_users=5 \
-    --global_ep=10 \
-    --learning_rate=1e-5 \
-    --num_lms 5 \
-    --training_type 1 \
-    --local_ep 10 \
-    --epochs 10 \
-    --N_Kmeans_update 10 \
-    --FL_STAGE 4 \
-    -model_out "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h" \
-    -model_in "/mnt/Internal/jieshiang/ADReSS-copy/save/data2vec-audio-large-960h" \
-    --dataset_path_root "/mnt/Internal/jieshiang/ADReSS-copy/datasets" \
-    --Kmeans_model_path "/mnt/Internal/jieshiang/ADReSS-copy/save/k_means_model" \
-    --eval_mode 2 \
-    --FL_type 1 \
-    --mu 0.01 \
-    --alpha 0.5 \
-    --beta 0.5 \
-    --train_batch_size 1 \
-    --eval_batch_size 1 \
-    --use_soft_clustering \
-    --fuzzy_m 2 \
-    --membership_threshold 0.175 \
-    --use_membership_weighted_avg \
-    # --use_rl_fcm \
-    # --FML_model 0 \
-    # --CBFL \
-    # --WeightedAvg \
-
-# FL algorithm types:
-# FL           : FL_type 1 / num_lms 1
-# FL_weighted  : FL_type 1 / num_lms 1 / WeightedAvg
-# FedProx      : FL_type 2 / num_lms 1
-# FML(Local)   : FL_type 3 / num_lms 1 / FML_model 0 
-# FML(mutual)  : FL_type 3 / num_lms 1 / FML_model 1
-# CBFL-embs    : FL_type 1 / num_lms 7 / CBFL
-# CBFL-CharDiv : FL_type 1 / num_lms 7
 
 # Handle SIGINT and SIGTERM for graceful termination of background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
